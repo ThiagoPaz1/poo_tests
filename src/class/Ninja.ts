639cb70e2aec8523e4901102fe64f1c3a9d7f378
Thiago <thiagopazjunior@gmail.com>
2024-10-11 00:18:36 -0300

    feat: WeaponNinja class created

--- src/class/Ninja.ts
+++ src/class/Ninja.ts
@@ -1,9 +1,13 @@
-export class Ninja {
+import { WaeponNinja } from "./WeaponNinja"
+
+export class Ninja extends WaeponNinja {
+  waepon: string = ""
   private name: string = ""
   private age: string = ""
   private village: string = ""
   
   constructor(name: string, age: string, village: string) {
+    super()
     this.name = name
     this.age = age
     this.village = village
@@ -12,8 +16,13 @@ export class Ninja {
   public get infoTheNinja(): string {
     return `Hello, my name is ${this.name}, i have ${this.age} and from at ${this.village} village`
   }
-
+  
   public atack(): string {
     return "Atack with jutsu"
+  
+  }
+  
+  getListWaepon(): string[] {
+    throw new Error("Method not implemented.")
   }
 }
---
+++ src/class/WeaponNinja.ts
@@ -0,0 +1,5 @@
+export abstract class WaeponNinja {
+  abstract waepon: string
+  abstract getListWaepon(): string[]
+}
+
